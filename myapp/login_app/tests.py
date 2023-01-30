cd212bd4c4cd232efa57705fadc6d14d13268b97
Guillaume <guillaumeclaes@yahoo.fr>
2023-01-30 22:04:45 +0100

    tests

--- myapp/login_app/tests.py
+++ myapp/login_app/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from .models import User, Register
+from .service import UserService, RegisterService
+import bcrypt
 
-# Create your tests here.
+class UserServiceTest(TestCase):
+    def setUp(self):
+        User.objects.create(username = "User1", email = "User1@gmail.com")
+        User.objects.create(username = "User2", email = "User2@gmail.com")
+
+    def test_find_user(self):
+        user_service = UserService()
+        user_found = user_service.find_user(start="Use")
+        self.assertEqual(len(user_found), 2)
+        self.assertEqual(user_found[0], "User1")
+        self.assertEqual(user_found[1], "User2")
+
+        user_found = user_service.find_user(start="Test")
+        self.assertEqual(len(user_found), 0)
+
+        user_found = user_service.find_user(start="User1")
+        self.assertEqual(len(user_found), 1)
+        self.assertEqual(user_found[0], "User1")
+
+    def test_find_user_by_username(self):
+        user_service = UserService()
+        user = user_service.find_user_by_username(username = "User1")
+        self.assertEqual(user.username, "User1")
+        self.assertEqual(user.email, "User1@gmail.com")
+
+class RegisterServiceTest(TestCase):
+    def setUp(self):
+        password = "crypted_password"
+        password:bytes = password.encode('utf-8')
+        hashed_password:bytes = bcrypt.hashpw(password, bcrypt.gensalt(10))
+        Register.objects.create(username = "User1", password = hashed_password.decode('utf-8'))
+
+    def test_register(self):
+        register_service = RegisterService()
+        register_service.register(username = "User2", password = "crypted_password", email = "test@gmail.com")
+        registered = Register.objects.filter(username = "User2").last()
+        user = User.objects.filter(username = "User2").last()
+        self.assertEqual(registered.username, "User2")
+        self.assertNotEqual(registered.password, "crypted_password")
+        self.assertEqual(user.username, "User2")
+        self.assertEqual(user.email, "test@gmail.com")
+
+    def test_authenticate(self):
+        register_service = RegisterService()
+        authenticated = register_service.authenticate(username = "User1", password = "crypted_password")
+        self.assertTrue(authenticated)
--- myapp/message_app/tests.py
+++ myapp/message_app/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from .models import Message
+from datetime import datetime
+from .service import MessageService
 
-# Create your tests here.
+class MessageServiceTest(TestCase):
+    def setUp(self):
+        Message.objects.create(content = "Test case1", emission_time = datetime(2022, 6, 14), author = "User1", target = "User2")
+        Message.objects.create(content = "Test case2", emission_time = datetime(2022, 6, 15), author = "User2", target = "User1")
+        Message.objects.create(content = "Test case3", emission_time = datetime(2022, 6, 16), author = "User1", target = "User2")
+        
+    def test_get_message(self):
+        message_service = MessageService()
+        messages = message_service.get_messages(author = "User1", target = "User2")
+        self.assertEqual(len(messages), 3)
+        self.assertEqual(messages[0].get_author(), "User1")
+        self.assertEqual(messages[0].get_target(), "User2")
+        self.assertEqual(messages[0].get_content(), "Test case1")
+        self.assertEqual(messages[1].get_author(), "User2")
+        self.assertEqual(messages[1].get_target(), "User1")
+        self.assertEqual(messages[1].get_content(), "Test case2")
+        self.assertEqual(messages[2].get_author(), "User1")
+        self.assertEqual(messages[2].get_target(), "User2")
+        self.assertEqual(messages[2].get_content(), "Test case3")
+
+    def test_create_message(self):
+        message_service = MessageService()
+        message_service.create_message(author = "User2", target = "User1", content = "Test case creation")
+        last_message = message_service.get_messages(author = "User1", target = "User2").last()
+        self.assertEqual(last_message.get_content(), "Test case creation")
+
+    def test_get_targets_and_last_messages(self):
+        message_service = MessageService()
+        target_message = message_service.get_targets_and_last_messages(user = "User1")
+        self.assertEqual(len(target_message), 1)
+        self.assertEqual(target_message[0].target, "User2")
+        self.assertEqual(target_message[0].message.content, "Test case3")
+
+    def test_get_targets_last_message(self):
+        message_service = MessageService()
+        target_messages = message_service.get_targets_last_message(user = "User1", targets = ("User1", "User2"))
+        self.assertEqual(len(target_messages), 2)
+        self.assertEqual(target_messages[0].target, "User1")
+        self.assertEqual(target_messages[0].message, None)
+        self.assertEqual(target_messages[1].target, "User2")
+        self.assertEqual(target_messages[1].message.content, "Test case3")
+
+    def test_count_message_sent_by_username(self):
+        message_service = MessageService()
+        self.assertEqual(message_service.count_message_sent_by_username(username = "User1"), 2)
+
+
+    def test_count_message_sent_to_username(self):
+        message_service = MessageService()
+        self.assertEqual(message_service.count_message_sent_to_username(username = "User1"), 1)
